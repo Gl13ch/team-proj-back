3f2eabaec2602471d0a0f9712a43fc522a4554f2
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-11-02 10:25:16 -0500

    full CRUD working

--- controllers/games.js
+++ controllers/games.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Games = require('../models/games.js')
 
 router.get('/', (req, res)=>{
-  res.send('Hello World')
+  Games.find({}, (err, foundpet) => {
+    res.json(foundpet)
+  })
+})
+
+router.post('/', (req, res) => {
+  Games.create(req.body, (err, createdpet) => {
+    res.json(createdpet)
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  Games.findByIdAndRemove(req.params.id, (err, deletedpet) => {
+    res.json(deletedpet)
+  })
+})
+
+router.put('/:id', (req, res) => {
+  Games.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedpet) => {
+    res.json(updatedpet)
+  })
 })
 
 module.exports = router
