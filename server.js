6705bcbdc3e5400a86c7b130a7b06a42996bfd2a
Kaylee Spindler <gl13ch@Kaylees-MacBook-Air.local>
2021-11-02 10:13:52 -0500

    controller set up

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
 const PORT = 3001
 
-app.get('/', (req, res)=>{
-  res.send('Hello')
-})
+app.use(express.json())
+app.use(cors())
+
+const gamesController = require('./controllers/games.js');
+app.use('/games', gamesController);
 
 app.listen(PORT, ()=>{
     console.log('listening...', PORT);
+})
+
+mongoose.connect('mongodb://localhost:27017/merncrud')
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongod...');
 });
